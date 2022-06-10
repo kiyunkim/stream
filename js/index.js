b50a1730c2bc427b75d1aa151592f0fd1af53988
kiyun <kiyunkim@users.noreply.github.com>
2022-06-09 20:40:25 -0400

    Set up http-server, readme, start setting up template for todo list

---
+++ js/index.js
@@ -0,0 +1,24 @@
+(function () {
+
+  const toDo = {};
+
+  // ---- Status:
+  // 0 = Not started
+  // 1 = Starting
+  // 2 = In progress
+  // 3 = QA
+  // 4 = In approval
+  // 5 = Done
+
+  toDo.work = {
+    645: {
+      desc: 'Description',
+      status: 0
+    }
+  }
+
+})();
+
+setTimeout(function () {
+  location.reload()
+}, 5000);
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Stream Overlays
+
+## How To Run During Stream
+
+1. Run `npx http-server [path] [options]`
+2. Visit `http://localhost:8080`
+
+OR
+
+1. `npm start`
+
+## Scenes
+
+1. To Do list for Co-working
